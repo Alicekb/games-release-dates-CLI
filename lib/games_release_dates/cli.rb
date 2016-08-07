ee540edaf5988c878187e49822f9b11fedbcf342
alice <github email address>
2016-08-07 14:34:34 -0400

    created way for user to learn more about a game

--- lib/games_release_dates/cli.rb
+++ lib/games_release_dates/cli.rb
@@ -35,7 +35,7 @@ class GamesReleaseDates::CLI
         February      June       October
         March         July       November
         April         August     December
-      ---------------------------------------------------
+      -----------------------------------------------------------
     eos
     month_input = gets.strip.upcase
 
@@ -47,12 +47,44 @@ class GamesReleaseDates::CLI
   end
 
   def list_releases(month, system)
+    @release_dates = []
+    overview_input = nil
+
     puts <<~eos
     *** #{month} - #{system} ***
-    ---------------------------------------------------
+    -----------------------------------------------------------
     eos
     @release_list.each do |games|
-      puts games if games.include?("#{system}") && games.include?("– #{month}")
+      @release_dates << games if games.include?("#{system}") && games.include?("– #{month}")
+    end
+
+    @release_dates.each_with_index do |game, index|
+      puts "#{index+1}. #{game}"
+    end
+
+    learn_more(system)
+  end
+
+  def learn_more(system)
+    puts <<~eos
+    -----------------------------------------------------------
+    Please enter the number of the game you would like to learn more about:
+    eos
+    overview_input = gets.to_i
+
+    if overview_input <= @release_dates.length && overview_input > 0
+
+      game_name = @release_dates[overview_input-1].gsub(/\([^\(]+/,"")
+      overview = GamesReleaseDates::Scraper.new.game_overview(game_name)
+
+      puts <<~eos
+      *** #{game_name} - #{system} ***
+      -----------------------------------------------------------
+      #{overview}
+      eos
+
+    else
+      learn_more
     end
   end
 end
